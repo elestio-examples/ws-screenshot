817bd29c9e3bf6cd735cca782fd9f41c95273700
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-01-21 11:59:45 +0200

    first

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# ws-screenshot
+# ws-screenshot CI/CD pipeline
+
+<a href="https://dash.elest.io/deploy?source=cicd&social=dockerCompose&url=https://github.com/elestio-examples/ws-screenshot"><img src="deploy-on-elestio.png" alt="Deploy on Elest.io" width="180px" /></a>
+
 Deploy ws-screenshot with CI/CD on Elestio
+
+<img src="ws-screenshot.gif" style='width: 100%;'/>
+<br/>
+<br/>
+
+# Once deployed ...
+
+You can open ws-screenshot UI here:
+
+    URL: https://[CI_CD_DOMAIN]
+    login: "root"
+    password: [ADMIN_PASSWORD]
+
+# Usage
+
+## REST API
+
+Make a GET request (or open the url in your browser):
+
+    https://[CI_CD_DOMAIN]/api/screenshot?resX=1280&resY=900&outFormat=jpg&isFullPage=false&url=https://vms2.terasp.net&headers={"foo":"bar"}
+
+## Websocket API
+
+    var event = {
+        cmd: "screenshot",
+        url: url,
+        originalTS: (+new Date()),
+        resX: resX,
+        resY: resY,
+        outFormat: outFormat,
+        isFullPage: isFullPage,
+        headers: {
+            foo: 'bar'
+        }
+    };
+
+You can check /public/js/client.js and /public/index.html for a sample on how to call the Websocket API
+
+# Supported parameters
+
+- url: full url to screenshot, must start with http:// or https://
+- resX: integer value for screen width, default: 1280
+- resY: integer value for screen height, default: 900
+- outFormat: output format, can be jpg, png or pdf, default: jpg
+- isFullPage: true or false, indicate if we should scroll the page and make a full page screenshot, default: false
+- waitTime: integer value in milliseconds, indicate max time to wait for page resources to load, default: 100
+- headers: add extra headers to the request
+
+# Protect with an ApiKey
+
+You can protect the REST & WS APIs with an ApiKey, this is usefull if you want to protect your screenshot server from being used by anyone To do that, open appconfig.json and set any string like a GUID in ApiKey attribute. This will be your ApiKey to pass to REST & WS APIs
+
+To call the REST API with an ApiKey:
+
+    https://[CI_CD_DOMAIN]/api/screenshot?url=https://example.com&apiKey=XXXXXXXXXXXXX
+
+To call the Websocket API with an ApiKey:
+
+    var event = {
+        cmd: "screenshot",
+        url: url,
+        originalTS: (+new Date()),
+        apiKey: "XXXXXXXXXXXXX"
+    };
+
+You can check /public/js/client.js for a sample on how to call the Websocket API
---
+++ public/js/client.js
@@ -0,0 +1,170 @@
+var head = document.getElementsByTagName("head")[0];
+
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js";
+script.onreadystatechange = handler;
+script.onload = handler;
+head.appendChild(script);
+
+function handler() {
+  connect();
+  UpdateRESTUrl();
+}
+
+function connect() {
+  var protocolPrefix = window.location.protocol === "https:" ? "wss:" : "ws:";
+  var rootURL = protocolPrefix + "//" + location.host;
+  var ws = new WebSocket(rootURL + "/ws/screenshot");
+  var nbConnectRetry = 5;
+  globalWS = ws;
+  ws.onopen = function () {
+    nbConnectRetry = 0;
+
+    //subscribe to ctx channel
+    Send({ cmd: "subscribe", channel: _wsTrail_ctx });
+
+    SendPendingQueue();
+  };
+
+  ws.onmessage = function (e) {
+    var msg = e.data;
+    //console.log('Message:', msg);
+
+    var obj = JSON.parse(msg);
+    if (obj.cmd == "responseScreenshot") {
+      if (obj.outFormat == "pdf") {
+        $("#resultPDF").attr("src", "data:application/pdf;base64, " + obj.data);
+        $("#resultPDF").css("width", document.getElementById("resX").value);
+        $("#resultPDF").css("height", document.getElementById("resY").value);
+
+        $("#resultPDF").show();
+        $("#resultImg").hide();
+      } else {
+        if (obj.data == "" && obj.details != null) {
+          alert("Error occured while taking the screenshot: " + obj.details);
+        } else {
+          $("#resultImg").attr("src", "data:image/jpeg;base64, " + obj.data);
+          $(".result-img-link").attr(
+            "href",
+            "data:image/jpeg;base64, " + obj.data
+          );
+          $("#resultImg").show();
+          $("#resultPDF").hide();
+        }
+      }
+
+      $("#stats").html(
+        "Processing time: " +
+          obj.execTime +
+          " - Roundtrip: " +
+          (+new Date() - obj.originalTS) +
+          "ms"
+      );
+      $("html,body").animate(
+        {
+          scrollTop: $("#stats").offset().top,
+        },
+        "slow"
+      );
+    } else {
+      $(_wsTrail_logSelector).append(sanitizeHTML(msg) + "<br/>");
+      $("body").scrollTop($("body").height());
+    }
+  };
+
+  ws.onclose = function (e) {
+    console.log(
+      "Socket is closed. Reconnect will be attempted in " +
+        nbConnectRetry +
+        " seconds.",
+      e.reason
+    );
+    setTimeout(function () {
+      connect();
+    }, 1000 * nbConnectRetry); //backoff mechanism
+  };
+
+  ws.onerror = function (err) {
+    console.error("Socket encountered error: ", err.message, "Closing socket");
+    ws.close();
+    nbConnectRetry += 1;
+  };
+}
+
+var sendQueue = [];
+function AskForScreenshot(
+  url,
+  resX,
+  resY,
+  outFormat,
+  isFullPage,
+  waitTime,
+  dismissModals
+) {
+  $("#resultImg").hide();
+  $("#stats").html("Please wait ...");
+  var event = {
+    cmd: "screenshot",
+    url: url,
+    originalTS: +new Date(),
+    resX: resX,
+    resY: resY,
+    outFormat: outFormat,
+    isFullPage: isFullPage,
+    waitTime: waitTime,
+    dismissModals: dismissModals,
+  };
+  Send(event);
+}
+
+function CleanMemory() {
+  var event = { cmd: "CleanMemory", originalTS: +new Date() };
+  Send(event);
+}
+
+function Send(event) {
+  if (globalWS.readyState != 1) {
+    //websocket is closed, add to the sendQueue, will be delivered when we are reconnected
+    sendQueue.push(event);
+    return;
+  }
+  globalWS.send(JSON.stringify(event));
+}
+
+function SendPendingQueue() {
+  //Send queued events
+  for (var i = 0; i < sendQueue.length; i++) {
+    Send(sendQueue[i]);
+  }
+  //clear the queue
+  sendQueue = [];
+}
+
+var sanitizeHTML = function (str) {
+  var temp = document.createElement("div");
+  temp.textContent = str;
+  return temp.innerHTML;
+};
+
+function UpdateRESTUrl() {
+  var baseUrl = "/api/screenshot?resX=" + $("#resX").val();
+  baseUrl += "&resY=" + $("#resY").val();
+  baseUrl += "&outFormat=" + $("#outFormat").val();
+  baseUrl += "&waitTime=" + $("#waitTime").val();
+  baseUrl += "&isFullPage=" + document.getElementById("isFullPage").checked;
+  baseUrl +=
+    "&dismissModals=" + document.getElementById("dismissModals").checked;
+
+  if ($("#fieldUrl").val().indexOf("&") > -1) {
+    baseUrl += "&url=" + encodeURIComponent($("#fieldUrl").val());
+  } else {
+    baseUrl += "&url=" + $("#fieldUrl").val();
+  }
+
+  $("#sampleREST").attr("href", baseUrl);
+  $("#sampleREST").text(baseUrl);
+}
+
+/////// USAGE //////
+// SendEvent("CREATE_PROJECT", "Title of this event ...", 1, {"toto": true, "tata": "yes", "test": 1230})
